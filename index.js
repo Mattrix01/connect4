194dacf780e289c7ed7807f26adee47110da446c
Mattrix01 <emjay.knight@gmail.com>
2022-12-20 15:34:13 +0000

    JS started and selectors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Listening out for when the dom content is loaded.
 document.addEventListener("DOMContentLoaded", () => {
-  //
+  const squares = document.querySelectorAll(".grid div");
+  const result = document.querySelector("#result");
+  const displayCurrentPlayer = document.querySelector("#current-player");
+  let currentPlayer = 1;
 });
-
-// 3:43:18
